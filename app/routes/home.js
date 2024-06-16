53e4dc7bf9fdf303b17f6b034cd302e0d38b993a
MusaKamau <141785937+MusaKamau@users.noreply.github.com>
2024-06-16 21:54:16 +0300

    Update home.js

--- app/routes/home.js
+++ app/routes/home.js
@@ -8,7 +8,7 @@ export const Home = ({ state, dispatch }) => html` <${Page}
     title="WEEKLY ECONOMIC NEWS"
     subtitle="By JAMES GITOGO"
     description="Explore the latest economic news and insights. Stay informed about global economic trends and developments."
-    sidebarImage=${prefixUriIfNeeded('WeeklyEconomist/assets/default-about.jpg')}
+    sidebarImage=${prefixUriIfNeeded('https://thepurpleclub.github.io/home/assets/harpy_eagle_1.jpg')}
     showLinks=${false}
 >
     <${PostsAndCategories} state=${state} dispatch=${dispatch} />
